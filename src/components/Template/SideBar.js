86b593102cb9639d3c854e846d77a4996331060f
Gauransh <picografix@gmail.com>
2022-08-22 16:21:07 +0530

    Dp Change

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -12,21 +12,16 @@ const SideBar = () => (
         <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael.l.dangelo@gmail.com">michael.l.dangelo@gmail.com</a></p>
+        <h2>Gauransh Soni</h2>
+        <p><a href="mailto:gauranshiitd@gmail.com">gauranshiitd@gmail</a></p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I like building things.
-        I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni, and
-        the co-founder and CTO of <a href="https://arthena.com">Arthena</a>. Before Arthena I was
-        at <a href="https://matroid.com">Matroid</a>
-        , <a href="https://planet.com">Planet</a>
-        , <a href="https://planetaryresources.com">Planetary Resources</a>
-        , <a href="https://facebook.com">Facebook</a>
-        , and <a href="https://seds.org">SEDS</a>.
+      <p>Hi, I&apos;m Gauransh. I like building innovative products.
+        I am a senior undergradute at <a href="https://iitd.ac.in">IIT Delhi</a>, and
+        upcoming Member of Technical Staff at <a href="https://adobe.com">Adobe Systems</a> India.
       </p>
       <ul className="actions">
         <li>
